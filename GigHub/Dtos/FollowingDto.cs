75990fbe10386a44ea3cc17ea55aab01906ef043
Jagbir <birchaudhary2000@gmail.com>
2019-12-14 23:37:35 +0930

    -remaining files

---
+++ GigHub/Dtos/FollowingDto.cs
@@ -0,0 +1,11 @@
+﻿using GigHub.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GigHub.Dtos
+{
+    public class FollowingDto
+    {
+        public string FolloweeId { get; set; }
+    }
+}
