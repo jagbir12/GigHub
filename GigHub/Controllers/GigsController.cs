257108ffaec8c01e08110c017c94b5f9a0bde8f0
Jagbir <birchaudhary2000@gmail.com>
2019-12-15 17:38:09 +0930

    - my upcoming gigs is added

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -17,6 +17,17 @@ namespace GigHub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g => g.Genre)
+                .ToList();
+            return View(gigs);
+        }
+
         public ActionResult Attending()
         {
             var userId = User.Identity.GetUserId();
@@ -68,7 +79,7 @@ namespace GigHub.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
